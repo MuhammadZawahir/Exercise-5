75fd09474e890163ef01300ceaee2e54fc4f711f
Zawahir <139978657+MuhammadZawahir@users.noreply.github.com>
2023-09-22 16:09:53 +0500

    Exercise 5

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famousquote = "Work, work and work and we are bound to succeed";
+let famousperson = "Quaid e Azam";
+console.log(`${famousperson} said:${famousquote}`);
